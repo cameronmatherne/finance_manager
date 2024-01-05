424b80f04dc1b43c8a18f1689a48be5fecdf5c73
Cameron <cameronpmatherne@gmail.com>
2024-01-05 16:18:23 -0600

    first commit

---
+++ api/server.js
@@ -0,0 +1,16 @@
+// api/server.js
+
+const cors = require("cors")
+app.use(cors())
+
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res) {
+  res.send("It's working!")
+})
+
+app.listen(3000, () => {
+  console.log("app listening on port 3000")
+})  
+
